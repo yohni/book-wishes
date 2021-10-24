9669d63eef5135ebc880181e80b397f68caa1a08
yohni <yohni.123@gmail.com>
2021-10-24 15:38:48 +0700

    feat: 🎸 contract config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
-function App() {
+function App({ contract, nearConfig }) {
+  console.log(nearConfig);
   return (
     <div className="App">
       <button className="btn btn-primary">Hallo</button>
