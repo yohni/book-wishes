8ce741cc29e89833cb67c24ef4d98de7dc5aa488
yohni <yohni.123@gmail.com>
2022-03-08 21:05:01 +0700

    fix: 🐛 clickable links

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,58 +1,84 @@
-import React from 'react';
+import React from "react";
+import { RiGithubFill, RiLinkedinBoxFill, RiMailFill } from "react-icons/ri";
 
 const Footer = () => {
   return (
     <div className="bg-gray-100">
-      <footer className="py-10 footer  text-base-content container">
+      <footer className="pt-10 pb-5 footer text-base-content container">
         <div>
-          <span className="footer-title">Services</span>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
+          <span className="footer-title">Techs</span>
+          <a
+            href="https://reactjs.org/"
+            className="link link-hover"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <a
+            href="https://near.org/"
+            className="link link-hover"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            NEAR
+          </a>
+          <a
+            href="https://daisyui.com/"
+            className="link link-hover"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Daisy UI
+          </a>
+          <a
+            href="https://tailwindcss.com"
+            className="link link-hover"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tailwind CSS
+          </a>
         </div>
         <div>
-          <span className="footer-title">Company</span>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <span className="footer-title">About</span>
+          <a
+            href="https://yohnifinan.com/about"
+            className="link link-hover"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Me
+          </a>
+          <a
+            href="https://yohnifinan.com"
+            className="link link-hover"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Portofolio
+          </a>
         </div>
         <div>
           <span className="footer-title">Social</span>
           <div className="grid grid-flow-col gap-4">
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-              </svg>
+            <a
+              className="hover:text-bookwishes"
+              href="https://twitter.com/yohni_finan"
+            >
+              <RiLinkedinBoxFill size={28} />
             </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
+            <a
+              className="hover:text-bookwishes"
+              href="https://github.com/yohni"
+            >
+              <RiGithubFill size={28} />
             </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-              </svg>
+            <a
+              className="hover:text-bookwishes"
+              href="mailto:yohni.123@gmail.com"
+            >
+              <RiMailFill size={28} />
             </a>
           </div>
         </div>
